dc123fd4994e26caf8f6de8f3b000dfa37fa8c7a
Shannon King <kingshannon01@gmail.com>
2019-10-08 13:49:13 -0700

    onChange setFilename

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -4,11 +4,17 @@ const FileUpload = () => {
   const [file, setFile] = useState("");
   const [filename, setFilename] = useState("Choose File");
 
+const onChange = e => {
+    setFile(e.target.files[0]); 
+    setFilename(e.target.files[0].name); 
+}
+
   return (
     <Fragment>
       <form>
         <div className="custom-file mb-4">
-          <input type="file" className="custom-file-input" id="customFile" />
+          <input type="file" className="custom-file-input" id="customFile" onChange=
+          {onChange} />
           <label className="custom-file-label" htmlFor="customFile">
             {filename}
           </label>
