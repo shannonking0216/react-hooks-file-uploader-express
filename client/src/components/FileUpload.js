484fcfebe18306c64bff4b55a0a2d4864a05acf6
Shannon King <kingshannon01@gmail.com>
2019-10-08 13:41:28 -0700

    added fileName

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,8 +1,8 @@
 import React, { Fragment, useState } from "react";
 
 const FileUpload = () => {
-    const [file, setFile] = useState();
-
+  const [file, setFile] = useState("");
+  const [filename, setFilename] = useState("Choose File");
 
   return (
     <Fragment>
@@ -10,11 +10,15 @@ const FileUpload = () => {
         <div className="custom-file mb-4">
           <input type="file" className="custom-file-input" id="customFile" />
           <label className="custom-file-label" htmlFor="customFile">
-            Choose file
+            {filename}
           </label>
         </div>
 
-        <input type="submit" value="Upload" className="btn btn-primary btn-block mt-4" />
+        <input
+          type="submit"
+          value="Upload"
+          className="btn btn-primary btn-block mt-4"
+        />
       </form>
     </Fragment>
   );
