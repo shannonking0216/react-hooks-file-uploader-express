727d4442946a8d3bcfb736e8b87df4894e126928
Shannon King <kingshannon01@gmail.com>
2019-10-08 14:09:30 -0700

    setUploadedFile

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 const App = () => (
   <div className="container mt-4">
     <h4 className="display-4 text-center mb-4">
-      <i className="fab fa-react" /> React file Upload
+      <i className="fab fa-react"></i> React file Upload
     </h4>
 
     <FileUpload />
--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,20 +1,49 @@
 import React, { Fragment, useState } from "react";
+import axios from "axios";
 
 const FileUpload = () => {
   const [file, setFile] = useState("");
   const [filename, setFilename] = useState("Choose File");
+  const [uploadedFile, setUploadedFile] = useState({});
 
-const onChange = e => {
-    setFile(e.target.files[0]); 
-    setFilename(e.target.files[0].name); 
-}
+  const onChange = e => {
+    setFile(e.target.files[0]);
+    setFilename(e.target.files[0].name);
+  };
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    const formData = new FromData();
+    formData.append("file", file);
+
+    try {
+      const res = await axios.post("/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      });
+      const { fileName, filePath } = res.data;
+
+      setUploadedFile({ fileName, filePath });
+    } catch (err) {
+      if (err.response.status === 500) {
+        console.log("There was a problem with the server");
+      } else {
+        console.log(err.response.data.msg);
+      }
+    }
+  };
 
   return (
     <Fragment>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="custom-file mb-4">
-          <input type="file" className="custom-file-input" id="customFile" onChange=
-          {onChange} />
+          <input
+            type="file"
+            className="custom-file-input"
+            id="customFile"
+            onChange={onChange}
+          />
           <label className="custom-file-label" htmlFor="customFile">
             {filename}
           </label>
